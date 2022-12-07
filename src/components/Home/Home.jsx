6d75baa23041f1e14d09c01bdc2bddd10c56887b
Tomislav Busic <tomislav.busic89@gmail.com>
2022-12-07 20:29:54 +0100

    start

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.scss';
 import axios from 'axios';
 
 const Home = () => {
+    const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
     
     const fetchingData = async () => {
+        setLoading(true);
         try {
             const response = await axios.get('https://api.escuelajs.co/api/v1/categories');
             console.log(response.data);
@@ -13,6 +17,7 @@ const Home = () => {
         } catch(error) {
             console.log(error);
         }
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -24,6 +29,23 @@ const Home = () => {
         <h1>Home</h1>
         <br />
         
+        <div className='cards-container'>
+            { loading && <h1>Loading...</h1> }
+
+            { !loading && 
+                data.map((product) => {
+                    return (
+                        <Card style={{ width: '18rem', margin: '1rem' }}>
+                            <Card.Img variant="top" src={ product.image } />
+                            <Card.Body>
+                              <Card.Title>{ product.name }</Card.Title>
+                              <Button variant="primary">See More</Button>
+                            </Card.Body>
+                        </Card>
+                    )
+                })
+            }
+        </div>
     </div>
   )
 }
