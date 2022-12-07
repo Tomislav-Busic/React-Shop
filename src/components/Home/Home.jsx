e605ebadfdbae510b031efe3a217aec1c51cbe74
Tomislav Busic <tomislav.busic89@gmail.com>
2022-12-07 20:13:55 +0100

    fetching data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,27 @@ import './Home.scss';
 import axios from 'axios';
 
 const Home = () => {
+    const [data, setData] = useState([]);
+    
+    const fetchingData = async () => {
+        try {
+            const response = await axios.get('https://api.escuelajs.co/api/v1/categories');
+            console.log(response.data);
+            setData(response.data);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchingData();
+    }, [])
+
   return (
     <div className='home'>
         <h1>Home</h1>
+        <br />
+        
     </div>
   )
 }
